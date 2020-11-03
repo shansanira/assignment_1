5a4cb1898255209132357acc397c3550978757a6
shansanira <shansanira@gmail.com>
2020-11-04 02:26:39 +0530

    add accordian

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavPanel from './components/nav-panel/nav-panel';
 import TopBox from "./components/top-box/top-box";
 import Breadcrumb from './components/breadcrumb/breadcrumb';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import CustomAccordian from './components/accordian/accordian';
 
 function App() {
   return (
@@ -30,6 +31,17 @@ function App() {
             <div className="col-sm-12">
               <p className="mt-4 mb-0">Current Risk Ownership</p>
               <Breadcrumb />
+            </div>
+
+            <div className="col-sm-12 d-flex justify-content-end mb-5">
+              <button type="button" className="btn btn-primary ml-2 custom-btn">Create Note</button>
+              <button type="button" className="btn btn-primary ml-2 custom-btn">Create Activity</button>
+              <button type="button" className="btn btn-primary ml-2 custom-btn">Re-Assign</button>
+              <button type="button" className="btn btn-primary ml-2 custom-btn">Update Recovery</button>
+            </div>
+
+            <div className="col-sm-12">
+              <CustomAccordian />
             </div>
           </div>
         </div>
