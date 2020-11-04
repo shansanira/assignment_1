4f0c87656d59179f2a5cdea8c4aa985705be1974
shansanira <shansanira@gmail.com>
2020-11-04 14:37:03 +0530

    data binding to timeline

--- src/components/accordian/accordian.js
+++ src/components/accordian/accordian.js
@@ -2,14 +2,15 @@ import { React } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import Timeline from './timeline';
+import './timeline.scss';
 
 const CustomAccordian = ({ data }) => (
     <Accordion>
         {
             data && data.map(item => (
                 <Card key={item.id}>
-                    <Accordion.Toggle as={Card.Header} eventKey={item.id}>
-                        {item.title}
+                    <Accordion.Toggle as={Card.Header} eventKey={item.id} className="card-header">
+                        <h6>{item.title}</h6>
                     </Accordion.Toggle>
                     <Accordion.Collapse eventKey={item.id}>
                         <Card.Body>
--- src/components/accordian/timeline.js
+++ src/components/accordian/timeline.js
@@ -4,50 +4,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Timeline = ({ timelineData }) => (
     <div className="time-line">
-        <div className="time-line-item">
-            <div className="time-line-icon-container">
-                <div className="time-line-icon">
-                    <FontAwesomeIcon className="icon" icon={['fas', 'file-alt']} fixedWidth />
+        {
+            timelineData && timelineData.map(item => (
+                <div className="time-line-item" key={item.id}>
+                    <div className="time-line-icon-container">
+                        <div className="time-line-icon">
+                            <FontAwesomeIcon className="icon" icon={['fas', 'file-alt']} fixedWidth />
+                        </div>
+                        <div className="vertical-line"></div>
+                    </div>
+                    <div className="time-line-content">
+                        <div className="time-line-content--title mb-5">
+                            <h4 className="m-0">{item.itemTitle}</h4>
+                            <span>{item.id}</span>
+                        </div>
+                        <div className="time-line-content--desc">
+                            <p className="dec-item">From: {item.from}</p>
+                            <p className="dec-item">To: {item.to}</p>
+                            <p className="dec-item">Participants: {item.participants}</p>
+                            <p className="dec-item">Loaction: {item.location}</p>
+                            <p className="dec-item">Remarks: {item.remarks}</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="vertical-line"></div>
-            </div>
-            <div className="time-line-content">
-                <div className="time-line-content--title mb-5">
-                    <h4 className="m-0">Followup Call</h4>
-                    <span>605875080</span>
-                </div>
-                <div className="time-line-content--desc">
-                    <p className="dec-item">From: 2020-07-24</p>
-                    <p className="dec-item">To: 2020-10-24</p>
-                    <p className="dec-item">Participants: -</p>
-                    <p className="dec-item">Loaction: Kandy</p>
-                    <p className="dec-item">Remarks: SR-Served</p>
-                </div>
-            </div>
-        </div>
-
-        <div className="time-line-item">
-            <div className="time-line-icon-container">
-                <div className="time-line-icon">
-                    <FontAwesomeIcon className="icon" icon={['fas', 'file-alt']} fixedWidth />
-                </div>
-                <div className="vertical-line"></div>
-            </div>
-            <div className="time-line-content">
-                <div className="time-line-content--title mb-5">
-                    <h4 className="m-0">Customer Visit</h4>
-                    <span>605875093</span>
-                </div>
-                <div className="time-line-content--desc">
-                    <p className="dec-item">From: 2020-07-23</p>
-                    <p className="dec-item">To: 2020-10-23</p>
-                    <p className="dec-item">Participants: -</p>
-                    <p className="dec-item">Loaction: Kandy</p>
-                    <p className="dec-item">Remarks: SR-Served</p>
-                </div>
-            </div>
-        </div>
-
+            ))
+        }
     </div>
 );
 
